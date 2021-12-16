9755a0e030de2f8990dd2a69f94a2e6294c3ef4b
Daniel Reynolds <danielreynolds101@gmail.com>
2021-12-15 19:58:21 -0700

    working

--- main.js
+++ main.js
@@ -6,16 +6,21 @@ const ctx = canvas.getContext('2d');
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
-const djGrid = new Grid([500,500], [1,50], 42)
+const seed = Math.floor(Math.random() * 1000)
+const djGrid = new Grid([500,500], [1,1000], seed, 0)
+const astarGrid = new Grid([500,500], [1,1000], seed,3)
 
 
 // for (let i = 0; i < 10; i++){
 
 // }
 djGrid.render(ctx, [0,0], [ctx.canvas.width / 2, ctx.canvas.height])
+astarGrid.render(ctx, [ctx.canvas.width / 2,0], [ctx.canvas.width / 2, ctx.canvas.height])
+
 setInterval(() => {
     for (let i = 0; i < 40; i++){
-        djGrid.step(ctx, [ctx.canvas.width / 2, ctx.canvas.height])
+        djGrid.step(ctx, [ctx.canvas.width / 2, ctx.canvas.height], [0,0])
+        astarGrid.step(ctx, [ctx.canvas.width / 2, ctx.canvas.height], [ctx.canvas.width / 2,0])
     }
     // djGrid.render(ctx, [0,0], [ctx.canvas.width / 2, ctx.canvas.height])
 },1)
--- src/Grid.js
+++ src/Grid.js
@@ -7,8 +7,9 @@ export default class Grid {
     //size in wxh
     //range in min->max
     //random seed can just be int
-    constructor(size, range, randomSeed) {
+    constructor(size, range, randomSeed, heurStrength) {
         // this.rng = new Math.seedrandom(randomSeed);
+        this.heurStrength = heurStrength;
         this.noise = noise.seed(randomSeed);
         this.grid = []
         this.size = size
@@ -17,24 +18,25 @@ export default class Grid {
         this.at = [0, 0]
         this.done = false;
         this.visited = new Set()
-        this.weightTo = {}
+        this.parents = {}
         for (let r = 0; r < size[1]; r++) {
             this.grid.push([])
             for (let c = 0; c < size[0]; c++) {
                 // console.log({yeet: (noise.perlin2(c / size[0], r / size[1]) + 1) / 2})
-                this.grid[r].push(Math.floor((noise.perlin2(c / 20, r / 20) + 1) / 2 * this.span) + range[0])
+                this.grid[r].push(Math.floor((noise.perlin2(c / 50, r / 50) + 1) / 2 * this.span) + range[0])
             }
         }
 
+        this.finishPoint = this.rowColToEncPoint(size[1] - 1, size[0] - 1)
         this.queue = new MinPriorityQueue({
             priority: (n) => {
-                return n.weight
+                return n.weight + n.heuristic
             }
         });
 
         // this.visited.add(0)
         // this.weightTo[0] = 0
-        this.queue.enqueue({ point: 0, weight: 0 })
+        this.queue.enqueue({ point: 0, weight: 0, heuristic: 0, parent: null })
     }
 
     render(ctx, topLeft, renderedSize) {
@@ -68,9 +70,10 @@ export default class Grid {
     heurToGoal(r, c) {
         const y = Math.abs(this.size[0] - r)
         const x = Math.abs(this.size[1] - c)
-        
-        // return 0
-        return (x+y) / 5000
+
+        // console.log(Math.sqrt(Math.pow(x,2) + Math.pow(y,2)))
+        // return 1000
+        return Math.sqrt(Math.pow(x,2) + Math.pow(y,2)) * this.heurStrength
     }
 
     getUnvisitedAdjacent(cur) {
@@ -79,35 +82,31 @@ export default class Grid {
 
 
         const points = []
-        if (1 <= r) {
-            const p = this.rowColToEncPoint(r - 1, c)
-            if (!this.visited.has(p)) {
-                points.push({ weight: cur.weight + (this.grid[r - 1][c] - this.grid[r][c]) + this.heurToGoal(r-1,c), point: p })
+        for (let tr of [-1, 0, 1]) {
+            if (!(0 <= r + tr && r + tr < this.size[1])) {
+                continue;
+            }
+            for (let tc of [-1, 0, 1]) {
+                if (!(0 <= c + tc && c + tc < this.size[0])) {
+                    continue;
+                }
+                if (tr === 0 && tc === 0) {
+                    continue
+                }
+                const p = this.rowColToEncPoint(r + tr, c + tc)
+                if (!this.visited.has(p)) {
+                    points.push({ weight: cur.weight + Math.max(this.grid[r+tr][c+tc] - this.grid[r][c], 0), point: p, heuristic: this.heurToGoal(r+tr, c+tc) })
+                }
             }
         }
-        if (r < this.size[1] - 1) {
-            const p = this.rowColToEncPoint(r + 1, c)
-            if (!this.visited.has(p)) {
-                points.push({ weight: cur.weight + (this.grid[r + 1][c] - this.grid[r][c]) + this.heurToGoal(r+1,c), point: p })
-            }
-        }
-
-        if (1 <= c) {
-            const p = this.rowColToEncPoint(r, c - 1)
-            if (!this.visited.has(p)) {
-                points.push({ weight: cur.weight + (this.grid[r][c-1] - this.grid[r][c]) + this.heurToGoal(r,c-1), point: p })
-            }
-        }
-        if (c < this.size[0] - 1) {
-            const p = this.rowColToEncPoint(r, c+1)
-            if (!this.visited.has(p)) {
-                points.push({ weight: cur.weight + (this.grid[r][c+1] - this.grid[r][c]) + this.heurToGoal(r,c+1), point: p })
-            }
-        }
+        
         return points
     }
 
-    step(ctx, renderedSize) {
+    step(ctx, renderedSize, topLeft) {
+        if (this.done) {
+            return;
+        }
         const wpp = renderedSize[0] / this.size[0]
         const hpp = renderedSize[1] / this.size[1]
         if (!this.queue.isEmpty()) {
@@ -115,15 +114,16 @@ export default class Grid {
             // arr = arr.sort((a,b) => a[1] - b[1])
             let current = this.queue.dequeue()
             // console.log({current})
-            while(this.visited.has(current.element.point)) {
+            while (this.visited.has(current.element.point)) {
                 current = this.queue.dequeue()
             }
+            this.parents[current.element.point] = current.element.parent;
             // console.log(this.queue.size())
             // console.log(arr[0])
             // const current = Number(arr[0][0])
-            const {r,c} = this.encPointToRowCol(current.element.point)
+            const { r, c } = this.encPointToRowCol(current.element.point)
             ctx.fillStyle = `black`;
-            ctx.fillRect(c * wpp, r * hpp, wpp, hpp);
+            ctx.fillRect(topLeft[0] + c * wpp, topLeft[1] + r * hpp, wpp, hpp);
             // const weightCurrent = this.weightTo[arr[0][0]];
             // delete this.weightTo[arr[0][0]]
             // if(current.priority !== this.weightTo[current.element.point]){
@@ -131,8 +131,23 @@ export default class Grid {
             // }
             this.visited.add(current.element.point)
             const adj = this.getUnvisitedAdjacent(current.element)
-            for(const a of adj){
-                this.queue.enqueue({point: a.point, weight: a.weight})
+            for (const a of adj) {
+                this.queue.enqueue({ point: a.point, weight: a.weight, heuristic: a.heuristic, parent: current.element.point })
+                if (a.point === this.finishPoint) {
+                    this.done = true;
+                    this.parents[a.point] = current.element.point;
+                    let backpath = a.point
+                    ctx.fillStyle = `white`;
+                    while (backpath != null) {
+                        const { r, c } = this.encPointToRowCol(backpath)
+                        const rb = r 
+                        const cb = c
+                        ctx.fillRect(topLeft[0] + cb * wpp, topLeft[1] + rb * hpp, wpp, hpp);
+                        backpath = this.parents[backpath]
+                    }
+                    console.log(this.parents[current.element.point])
+                    
+                }
             }
             // console.log(this.queue.size())
         }
